8e341351e61ee2210868c97ddf85cdee07d1ee52
kashifsudo <kashifnezam10@gmail.com>
2026-03-10 10:56:50 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,223 @@
+const questions = [
+
+{
+question: "Who founded the Maurya Empire?",
+options: [
+"A. Chandragupta Maurya",
+"B. Ashoka",
+"C. Bindusara",
+"D. Bimbisara"
+],
+answer: 0
+},
+
+{
+question: "The capital of Magadha during Bimbisara's rule was:",
+options:[
+"A. Vaishali",
+"B. Rajgir",
+"C. Pataliputra",
+"D. Nalanda"
+],
+answer:1
+},
+
+{
+question:"Nalanda University was established during the reign of:",
+options:[
+"A. Harsha",
+"B. Chandragupta II",
+"C. Kumaragupta",
+"D. Skandagupta"
+],
+answer:2
+},
+
+{
+question:"The Battle of Buxar was fought in:",
+options:[
+"A. 1757",
+"B. 1764",
+"C. 1775",
+"D. 1782"
+],
+answer:1
+},
+
+{
+question:"Who was the founder of the Gupta Dynasty?",
+options:[
+"A. Chandragupta I",
+"B. Samudragupta",
+"C. Sri Gupta",
+"D. Kumaragupta"
+],
+answer:2
+},
+
+{
+question:"Ashoka embraced Buddhism after which battle?",
+options:[
+"A. Battle of Panipat",
+"B. Battle of Kalinga",
+"C. Battle of Hydaspes",
+"D. Battle of Buxar"
+],
+answer:1
+},
+
+{
+question:"Who wrote 'Arthashastra'?",
+options:[
+"A. Chanakya",
+"B. Kalidasa",
+"C. Megasthenes",
+"D. Panini"
+],
+answer:0
+},
+
+{
+question:"The ancient name of Patna was:",
+options:[
+"A. Kusumpura",
+"B. Pataliputra",
+"C. Both A and B",
+"D. Rajgriha"
+],
+answer:2
+},
+
+{
+question:"Who was the Greek ambassador in Chandragupta Maurya's court?",
+options:[
+"A. Seleucus",
+"B. Megasthenes",
+"C. Herodotus",
+"D. Pliny"
+],
+answer:1
+},
+
+{
+question:"The Vikramshila University was founded by:",
+options:[
+"A. Dharmapala",
+"B. Ashoka",
+"C. Harsha",
+"D. Chandragupta II"
+],
+answer:0
+},
+
+{
+question:"Sher Shah Suri was born in:",
+options:[
+"A. Sasaram",
+"B. Patna",
+"C. Ara",
+"D. Gaya"
+],
+answer:0
+},
+
+{
+question:"The Grand Trunk Road was built by:",
+options:[
+"A. Akbar",
+"B. Sher Shah Suri",
+"C. Babur",
+"D. Humayun"
+],
+answer:1
+},
+
+{
+question:"The Gupta period is known as:",
+options:[
+"A. Golden Age of India",
+"B. Dark Age",
+"C. Age of Trade",
+"D. Iron Age"
+],
+answer:0
+},
+
+{
+question:"Who composed the Allahabad Pillar Inscription?",
+options:[
+"A. Kalidasa",
+"B. Harisena",
+"C. Banabhatta",
+"D. Panini"
+],
+answer:1
+},
+
+{
+question:"The capital of the Gupta Empire was:",
+options:[
+"A. Ujjain",
+"B. Pataliputra",
+"C. Mathura",
+"D. Kannauj"
+],
+answer:1
+},
+
+{
+question:"The Buddhist council at Vaishali was:",
+options:[
+"A. First",
+"B. Second",
+"C. Third",
+"D. Fourth"
+],
+answer:1
+},
+
+{
+question:"Who defeated Ibrahim Lodi in 1526?",
+options:[
+"A. Babur",
+"B. Akbar",
+"C. Sher Shah",
+"D. Humayun"
+],
+answer:0
+},
+
+{
+question:"The famous Nalanda University is located in which state?",
+options:[
+"A. Uttar Pradesh",
+"B. Bihar",
+"C. Madhya Pradesh",
+"D. West Bengal"
+],
+answer:1
+},
+
+{
+question:"Who wrote 'Rajatarangini'?",
+options:[
+"A. Kalhana",
+"B. Banabhatta",
+"C. Harisena",
+"D. Panini"
+],
+answer:0
+},
+
+{
+question:"Who was the last Mauryan ruler?",
+options:[
+"A. Ashoka",
+"B. Brihadratha",
+"C. Dasharatha",
+"D. Samprati"
+],
+answer:1
+}
+
+];
